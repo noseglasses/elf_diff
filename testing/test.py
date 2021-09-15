7df491b3c7cfb03d2e17aabddcf96cff98f10666
noseglasses <shinynoseglasses@gmail.com>
2021-09-15 09:46:43 +0200

    Update CI tests

--- testing/test.py
+++ testing/test.py
@@ -37,144 +37,163 @@ new_binary2 = testing_dir + "/libelf_diff_test_debug_new.a"
 
 verbose_output = True
 
-def runSubprocess(cmd, cwd = os.getcwd(), env = os.environ.copy()):
-   
-   if verbose_output:
-      print("   Running command: {cmd}".format(cmd = "\"" + "\" \"".join(cmd) + "\""))
-   
-   try:
-      proc = subprocess.Popen(cmd, \
-                              stdout = subprocess.PIPE, \
-                              stderr = subprocess.PIPE, \
-                              cwd = cwd, \
-                              env = env)
-      
-      o, e = proc.communicate()
-      
-      rc = proc.returncode
-         
-      output = o.decode("utf8")
-      error = e.decode("utf8")
-      
-   except (OSError) as e:
-      print(e)
-      sys.exit(1)
-   
-   if rc != 0:
-      print("Failed running command %s in directory %s failed" % (str(cmd), cwd))
-      print("   exit code: %d" % (rc))
-      print("   stdout: %s" % (output))
-      print("   stderr: %s" % (error))
-      sys.exit(1)
-      
-   if verbose_output:
-      print("      exit code: %d" % (rc))
-      if len(output) > 0:
-         print("      stdout: %s" % (output))
-         
-      if len(error) > 0:
-         print("      stderr: %s" % (error))
-      
-   return [output, error]
-   
+
+def runSubprocess(cmd, cwd=os.getcwd(), env=os.environ.copy()):
+
+    if verbose_output:
+        print("   Running command: {cmd}".format(cmd='"' + '" "'.join(cmd) + '"'))
+
+    try:
+        proc = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=env
+        )
+
+        o, e = proc.communicate()
+
+        rc = proc.returncode
+
+        output = o.decode("utf8")
+        error = e.decode("utf8")
+
+    except (OSError) as e:
+        print(e)
+        sys.exit(1)
+
+    if rc != 0:
+        print("Failed running command %s in directory %s failed" % (str(cmd), cwd))
+        print("   exit code: %d" % (rc))
+        print("   stdout: %s" % (output))
+        print("   stderr: %s" % (error))
+        sys.exit(1)
+
+    if verbose_output:
+        print("      exit code: %d" % (rc))
+        if len(output) > 0:
+            print("      stdout: %s" % (output))
+
+        if len(error) > 0:
+            print("      stderr: %s" % (error))
+
+    return [output, error]
+
+
 class TestCommandLineArgs(unittest.TestCase):
-   def test_pair_report(self):
-      
-      pdf_file = "pair_report_output.pdf"
-      html_file = "pair_report_output.html"
-      template_file = "output.tmpl.yml"
-       
-      [output, error] = \
-          runSubprocess([sys.executable, elf_diff_executable, \
-                      "--old_binary_filename", old_binary, \
-                      "--new_binary_filename", new_binary, \
-                      "--old_alias", "an_old_alias", \
-                      "--new_alias", "a_new_alias", \
-                      "--old_info_file", testing_dir + "/old_binary_info.txt", \
-                      "--new_info_file", testing_dir + "/new_binary_info.txt", \
-                      "--build_info", "General build info", \
-                      "--html_file", html_file, \
-                      "--pdf_file", pdf_file, \
-                      "--project_title", "Project title", \
-                      "--driver_template_file", template_file])
-       
-      # Check if all output files exist
-      #
-      self.assertTrue(os.path.isfile(pdf_file))
-      self.assertTrue(os.path.isfile(html_file))
-      self.assertTrue(os.path.isfile(template_file))
-       
-   def test_driven_pair_report(self):
-      
-      driver_yaml_file = "pair_report.driver.yml"
-      
-      pdf_file = "driven_pair_report_output.pdf"
-      html_file = "driven_pair_report_output.html"
-      template_file = "driven_pair_report_output.tmpl.yml"
-      
-      with open(driver_yaml_file, "w") as f:
-         
-         f.write("old_binary_filename: " + old_binary + "\n")
-         f.write("new_binary_filename: " + new_binary + "\n")
-         f.write("old_alias: " + "an_old_alias\n")
-         f.write("new_alias: " + "a_new_alias\n")
-         f.write("old_info_file: " + testing_dir + "/old_binary_info.txt\n")
-         f.write("new_info_file: " + testing_dir + "/new_binary_info.txt\n")
-         f.write("build_info: >\n")
-         f.write("  Build info.\n")
-         f.write("  More build info.\n")
-         f.write("html_file: " + html_file + "\n")
-         f.write("pdf_file: " + pdf_file + "\n")
-         f.write("project_title: " + "Project title\n")
-         f.write("driver_template_file: " + template_file + "\n")
-         
-      [output, error] = \
-          runSubprocess([sys.executable, elf_diff_executable, \
-                      "--driver_file", driver_yaml_file])
-       
-      # Check if all output files exist
-      #
-      self.assertTrue(os.path.isfile(pdf_file))
-      self.assertTrue(os.path.isfile(html_file))
-      self.assertTrue(os.path.isfile(template_file))
-       
-   def test_driven_mass_report(self):
-      
-      driver_yaml_file = "mass_report.driver.yml"
-            
-      pdf_file = "driven_pair_report_output.pdf"
-      html_file = "driven_pair_report_output.html"
-      template_file = "driven_mass_report_output.tmpl.yml"
-      
-      with open(driver_yaml_file, "w") as f:
-         
-         f.write("build_info: >\n")
-         f.write("  Build info.\n")
-         f.write("  More build info.\n")
-         f.write("html_file: " + html_file + "\n")
-         f.write("pdf_file: " + pdf_file + "\n")
-         f.write("project_title: " + "Project title\n")
-         f.write("driver_template_file: " + template_file + "\n")
-         
-         f.write("binary_pairs:\n")
-         f.write("    - old_binary: \"" + old_binary + "\"\n")
-         f.write("      new_binary: \"" + new_binary + "\"\n")
-         f.write("      short_name: \"First binary name\"\n")
-         
-         #f.write("binary_pairs:\n")
-         f.write("    - old_binary: \"" + old_binary2 + "\"\n")
-         f.write("      new_binary: \"" + new_binary2 + "\"\n")
-         f.write("      short_name: \"Second binary name\"\n")
-         
-      [output, error] = \
-          runSubprocess([sys.executable, elf_diff_executable, \
-                      "--driver_file", driver_yaml_file])
-       
-      # Check if all output files exist
-      #
-      self.assertTrue(os.path.isfile(pdf_file))
-      self.assertTrue(os.path.isfile(html_file))
-      self.assertTrue(os.path.isfile(template_file))
-      
-if __name__ == '__main__':
+    def test_pair_report(self):
+
+        pdf_file = "pair_report_output.pdf"
+        html_file = "pair_report_output.html"
+        html_dir = "pair_report_multi_page"
+        template_file = "output.tmpl.yml"
+
+        [output, error] = runSubprocess(
+            [
+                sys.executable,
+                elf_diff_executable,
+                "--old_binary_filename",
+                old_binary,
+                "--new_binary_filename",
+                new_binary,
+                "--old_alias",
+                "an_old_alias",
+                "--new_alias",
+                "a_new_alias",
+                "--old_info_file",
+                testing_dir + "/old_binary_info.txt",
+                "--new_info_file",
+                testing_dir + "/new_binary_info.txt",
+                "--build_info",
+                "General build info",
+                "--html_file",
+                html_file,
+                "--html_dir",
+                html_dir,
+                "--pdf_file",
+                pdf_file,
+                "--project_title",
+                "Project title",
+                "--driver_template_file",
+                template_file,
+            ]
+        )
+
+        # Check if all output files exist
+        #
+        self.assertTrue(os.path.isfile(pdf_file))
+        self.assertTrue(os.path.isfile(html_file))
+        self.assertTrue(os.path.isfile(template_file))
+
+    def test_driven_pair_report(self):
+
+        driver_yaml_file = "pair_report.driver.yml"
+
+        pdf_file = "driven_pair_report_output.pdf"
+        html_file = "driven_pair_report_output.html"
+        html_dir = "driven_pair_report_multi_page"
+        template_file = "driven_pair_report_output.tmpl.yml"
+
+        with open(driver_yaml_file, "w") as f:
+
+            f.write("old_binary_filename: " + old_binary + "\n")
+            f.write("new_binary_filename: " + new_binary + "\n")
+            f.write("old_alias: " + "an_old_alias\n")
+            f.write("new_alias: " + "a_new_alias\n")
+            f.write("old_info_file: " + testing_dir + "/old_binary_info.txt\n")
+            f.write("new_info_file: " + testing_dir + "/new_binary_info.txt\n")
+            f.write("build_info: >\n")
+            f.write("  Build info.\n")
+            f.write("  More build info.\n")
+            f.write("html_file: " + html_file + "\n")
+            f.write("html_dir: " + html_dir + "\n")
+            f.write("pdf_file: " + pdf_file + "\n")
+            f.write("project_title: " + "Project title\n")
+            f.write("driver_template_file: " + template_file + "\n")
+
+        [output, error] = runSubprocess(
+            [sys.executable, elf_diff_executable, "--driver_file", driver_yaml_file]
+        )
+
+        # Check if all output files exist
+        #
+        self.assertTrue(os.path.isfile(pdf_file))
+        self.assertTrue(os.path.isfile(html_file))
+        self.assertTrue(os.path.isfile(template_file))
+
+    def test_driven_mass_report(self):
+
+        driver_yaml_file = "mass_report.driver.yml"
+
+        pdf_file = "driven_pair_report_output.pdf"
+        html_file = "driven_pair_report_output.html"
+        template_file = "driven_mass_report_output.tmpl.yml"
+
+        with open(driver_yaml_file, "w") as f:
+
+            f.write("build_info: >\n")
+            f.write("  Build info.\n")
+            f.write("  More build info.\n")
+            f.write("html_file: " + html_file + "\n")
+            f.write("pdf_file: " + pdf_file + "\n")
+            f.write("project_title: " + "Project title\n")
+            f.write("driver_template_file: " + template_file + "\n")
+
+            f.write("binary_pairs:\n")
+            f.write('    - old_binary: "' + old_binary + '"\n')
+            f.write('      new_binary: "' + new_binary + '"\n')
+            f.write('      short_name: "First binary name"\n')
+            f.write('    - old_binary: "' + old_binary2 + '"\n')
+            f.write('      new_binary: "' + new_binary2 + '"\n')
+            f.write('      short_name: "Second binary name"\n')
+
+        [output, error] = runSubprocess(
+            [sys.executable, elf_diff_executable, "--driver_file", driver_yaml_file]
+        )
+
+        # Check if all output files exist
+        #
+        self.assertTrue(os.path.isfile(pdf_file))
+        self.assertTrue(os.path.isfile(html_file))
+        self.assertTrue(os.path.isfile(template_file))
+
+
+if __name__ == "__main__":
     unittest.main()
--- testing/test_cpp_symbols.py
+++ testing/test_cpp_symbols.py
@@ -30,148 +30,149 @@ sys.path.append(bin_path + "/../python")
 from elf_diff.symbol import Symbol
 from elf_diff.symbol import CppSymbol
 
+
 class TestCppSymbol(unittest.TestCase):
-            
-   class __RAIITestAux(object):
-      def __init__(self, master_test):
-         self.master_test = master_test
-         for prop in CppSymbol.props:
-            setattr(self, prop, None)
-      
-      def run(self):
+    class __RAIITestAux(object):
+        def __init__(self, master_test):
+            self.master_test = master_test
+            for prop in CppSymbol.props:
+                setattr(self, prop, None)
 
-         # Establisch symbol name
-         self.name = ""
-         
-         if self.namespace is not None:
-            self.name = self.namespace
-            
-         if self.template_parameters is not None:
-            self.name += "<" + self.template_parameters + ">"
-            
-         if self.namespace is not None:
-            self.name += "::"
-            
-         self.name += self.full_name 
-         
-         if self.arguments is not None:
-            self.name += "(" + self.arguments + ")"
+        def run(self):
 
-         s = CppSymbol(self.name)
-         s.init()
-         
-         self.master_test.assertTrue(s.symbol_type == self.symbol_type)
-         
-         if not s.propertiesEqual(self):
-            s_props = s.getProperties()
-            print(f"{self.name} Properties mismatch")
-            for key, value in s_props.items():
-               self_value = getattr(self, key)
-               print(f"   {key}: expected {self_value}, actual {value}")
-         
-         self.master_test.assertTrue(s.propertiesEqual(self))
-      
-   def setUp(self):
-      self.template_parameters = "std::size_t, std::vector<int>"
-      
-   def __dataSymbolTest(self):
-      t = self.__RAIITestAux(self)
-      t.symbol_type = Symbol.type_data
-      return t
-      
-   def __functionSymbolTest(self):
-      t = self.__dataSymbolTest()
-      t.symbol_type = Symbol.type_function
-      return t
-         
-   def __simpleDataTest(self):
-      p = self.__dataSymbolTest()
-      p.full_name = "a"
-      return p
-   
-   def simpleDataTest(self):
-      self.__simpleDataTest().run()
-   
-   def testDataInNamespace(self):
-      p = self.__simpleDataTest()
-      p.namespace = "n"
-      p.run()
-   
-   def testDataInClassInNamespace(self):
-      p = self.__simpleDataTest()
-      p.namespace = "n::c"
-      p.run()
-   
-   def testDataInTemplateClass(self):
-      p = self.__simpleDataTest()
-      p.namespace = "c"
-      p.template_parameters = self.template_parameters
-      p.run()
-   
-   def testDataInClassInNamespace(self):
-      p = self.__simpleDataTest()
-      p.namespace = "n::c"
-      p.run()
-      
-   def testSimpleFunction(self):
-      p = self.__functionSymbolTest()
-      p.full_name = "f"
-      p.arguments = ""
-      p.run()
-   
-   def __simpleFunctionWithSimpleArgs(self):
-      p = self.__functionSymbolTest()
-      p.full_name = "f"
-      p.arguments = "int, double"
-      return p
-   
-   def __simpleFunctionWithTemplateArg(self):
-      p = self.__functionSymbolTest()
-      p.full_name = "f"
-      p.arguments = "std::vector<int>, double"
-      return p
-   
-   def __simpleFunctionWithTemplateArgWithBraces(self):
-      p = self.__functionSymbolTest()
-      p.full_name = "f"
-      p.arguments = "std::array<int, (foo)3>, double"
-      return p
-   
-   def __getFunctionTests(self):
-      return [
-         self.__simpleFunctionWithSimpleArgs(),
-         self.__simpleFunctionWithTemplateArg(),
-         self.__simpleFunctionWithTemplateArgWithBraces()
-      ]
-   
-   def testSimpleFunctions(self):
-      for test in self.__getFunctionTests():
-         test.run()
-   
-   def testFunctionsInNamespaces(self):
-      for test in self.__getFunctionTests():
-         test.namespace = "n"
-         test.run()
-      
-   def testFunctionsInClassesInNamespaces(self):
-      tests = self.__getFunctionTests()
-      for test in tests:
-         test.namespace = "n::c"
-         test.run()
-      
-   def testFunctionsInTemplateClasses(self):
-      tests = self.__getFunctionTests()
-      for test in tests:
-         test.namespace = "c"
-         test.template_parameters = self.template_parameters
-         test.run()
-      
-   def testFunctionsInTemplateClassesInNamespaces(self):
-      tests = self.__getFunctionTests()
-      for test in tests:
-         test.namespace = "n::c"
-         test.template_parameters = self.template_parameters
-         test.run()
-      
-if __name__ == '__main__':
+            # Establisch symbol name
+            self.name = ""
+
+            if self.namespace is not None:
+                self.name = self.namespace
+
+            if self.template_parameters is not None:
+                self.name += "<" + self.template_parameters + ">"
+
+            if self.namespace is not None:
+                self.name += "::"
+
+            self.name += self.full_name
+
+            if self.arguments is not None:
+                self.name += "(" + self.arguments + ")"
+
+            s = CppSymbol(self.name)
+            s.init()
+
+            self.master_test.assertTrue(s.symbol_type == self.symbol_type)
+
+            if not s.propertiesEqual(self):
+                s_props = s.getProperties()
+                print(f"{self.name} Properties mismatch")
+                for key, value in s_props.items():
+                    self_value = getattr(self, key)
+                    print(f"   {key}: expected {self_value}, actual {value}")
+
+            self.master_test.assertTrue(s.propertiesEqual(self))
+
+    def setUp(self):
+        self.template_parameters = "std::size_t, std::vector<int>"
+
+    def __dataSymbolTest(self):
+        t = self.__RAIITestAux(self)
+        t.symbol_type = Symbol.type_data
+        return t
+
+    def __functionSymbolTest(self):
+        t = self.__dataSymbolTest()
+        t.symbol_type = Symbol.type_function
+        return t
+
+    def __simpleDataTest(self):
+        p = self.__dataSymbolTest()
+        p.full_name = "a"
+        return p
+
+    def simpleDataTest(self):
+        self.__simpleDataTest().run()
+
+    def testDataInNamespace(self):
+        p = self.__simpleDataTest()
+        p.namespace = "n"
+        p.run()
+
+    def testDataInClassInNamespace(self):
+        p = self.__simpleDataTest()
+        p.namespace = "n::c"
+        p.run()
+
+    def testDataInTemplateClass(self):
+        p = self.__simpleDataTest()
+        p.namespace = "c"
+        p.template_parameters = self.template_parameters
+        p.run()
+
+    def testDataInClassInNamespace(self):
+        p = self.__simpleDataTest()
+        p.namespace = "n::c"
+        p.run()
+
+    def testSimpleFunction(self):
+        p = self.__functionSymbolTest()
+        p.full_name = "f"
+        p.arguments = ""
+        p.run()
+
+    def __simpleFunctionWithSimpleArgs(self):
+        p = self.__functionSymbolTest()
+        p.full_name = "f"
+        p.arguments = "int, double"
+        return p
+
+    def __simpleFunctionWithTemplateArg(self):
+        p = self.__functionSymbolTest()
+        p.full_name = "f"
+        p.arguments = "std::vector<int>, double"
+        return p
+
+    def __simpleFunctionWithTemplateArgWithBraces(self):
+        p = self.__functionSymbolTest()
+        p.full_name = "f"
+        p.arguments = "std::array<int, (foo)3>, double"
+        return p
+
+    def __getFunctionTests(self):
+        return [
+            self.__simpleFunctionWithSimpleArgs(),
+            self.__simpleFunctionWithTemplateArg(),
+            self.__simpleFunctionWithTemplateArgWithBraces(),
+        ]
+
+    def testSimpleFunctions(self):
+        for test in self.__getFunctionTests():
+            test.run()
+
+    def testFunctionsInNamespaces(self):
+        for test in self.__getFunctionTests():
+            test.namespace = "n"
+            test.run()
+
+    def testFunctionsInClassesInNamespaces(self):
+        tests = self.__getFunctionTests()
+        for test in tests:
+            test.namespace = "n::c"
+            test.run()
+
+    def testFunctionsInTemplateClasses(self):
+        tests = self.__getFunctionTests()
+        for test in tests:
+            test.namespace = "c"
+            test.template_parameters = self.template_parameters
+            test.run()
+
+    def testFunctionsInTemplateClassesInNamespaces(self):
+        tests = self.__getFunctionTests()
+        for test in tests:
+            test.namespace = "n::c"
+            test.template_parameters = self.template_parameters
+            test.run()
+
+
+if __name__ == "__main__":
     unittest.main()
