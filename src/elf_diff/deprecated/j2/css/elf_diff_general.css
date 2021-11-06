18d1b6561459c2e27a2b4d9c7735760f04d80a8b
noseglasses <shinynoseglasses@gmail.com>
2021-11-06 10:16:42 +0100

    style: Fix copy paste error in license text

--- src/elf_diff/deprecated/j2/css/elf_diff_general.css
+++ src/elf_diff/deprecated/j2/css/elf_diff_general.css
@@ -7,7 +7,7 @@ elf_diff
 
 Copyright (C) 2019  Noseglasses (shinynoseglasses@gmail.com)
 
-This program is free software: you can redistribute it and/or modify it under it under
+This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
 Foundation, version 3.
 
